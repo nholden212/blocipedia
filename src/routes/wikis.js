be2f81aa77ab2504f9ec42267f2b390e56e32201
Nick Holden <nholden212@gmail.com>
2018-10-07 13:38:18 -0700

    Created wiki index page view and navbar access

--- src/routes/wikis.js
+++ src/routes/wikis.js
@@ -2,6 +2,6 @@ const express = require("express");
 const router = express.Router();
 const wikiController = require("../controllers/wikiController");
 
-
+router.get("/wikis", wikiController.index);
 
 module.exports = router;
