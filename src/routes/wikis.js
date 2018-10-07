7a1623328dca4e048d4e39795b0c43e1d44d6aeb
Nick Holden <nholden212@gmail.com>
2018-10-07 14:36:07 -0700

    Implemented wiki Update

--- src/routes/wikis.js
+++ src/routes/wikis.js
@@ -4,9 +4,11 @@ const wikiController = require("../controllers/wikiController");
 
 router.get("/wikis", wikiController.index);
 router.get("/wikis/new", wikiController.new);
+router.get("/wikis/:id/edit", wikiController.edit);
 router.get("/wikis/:id", wikiController.show);
 
 router.post("/wikis/create", wikiController.create);
 router.post("/wikis/:id/destroy", wikiController.destroy);
+router.post("/wikis/:id/update", wikiController.update);
 
 module.exports = router;
