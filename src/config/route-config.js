9597de6c50937c33a3f7027f8e775878d8469bc7
Nick Holden <nholden212@gmail.com>
2018-10-06 17:18:43 -0700

    User sign up working without sendgrid

--- src/config/route-config.js
+++ src/config/route-config.js
@@ -2,8 +2,10 @@ module.exports = {
   init(app){
 
     const staticRoutes = require("../routes/static");
+    const userRoutes = require("../routes/user");
 
     app.use(staticRoutes);
+    app.use(userRoutes);
 
   }
 }
