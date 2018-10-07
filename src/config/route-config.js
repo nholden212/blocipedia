e5136edc54d2b0dc86860f225bc2afaada7d4037
Nick Holden <nholden212@gmail.com>
2018-10-07 13:15:48 -0700

    Created wiki resource model and migration

--- src/config/route-config.js
+++ src/config/route-config.js
@@ -3,9 +3,11 @@ module.exports = {
 
     const staticRoutes = require("../routes/static");
     const userRoutes = require("../routes/user");
+    const wikiRoutes = require("../routes/wikis");
 
     app.use(staticRoutes);
     app.use(userRoutes);
+    app.use(wikiRoutes);
 
   }
 }
---
+++ src/routes/wikis.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const router = express.Router();
+const wikiController = require("../controllers/wikiController");
+
+
+
+module.exports = router;
